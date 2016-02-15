6ad511aa7b3d5de3d8d8986a7bac98aaa1d7b60e
James <jammatpic@gmail.com>
2016-02-16 02:49:11 +1100

    Added some API info and rough HTML template

--- resources/js/twitch-viewer.js
+++ resources/js/twitch-viewer.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
+var steamers  = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 
+$(document).ready(function() {
+    $.getJSON('https://api.twitch.tv/kraken/streams/ESL_SC2?callback=?', function(data) {
+        console.log(JSON.stringify(data));
+    });
 });
